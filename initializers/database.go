3197183a03b5d800f0189be57020e5f5818c01c6
jicodes <liu.ji1@northeastern.edu>
2024-02-07 11:32:09 -0800

    add database initializer

---
+++ initializers/database.go
@@ -0,0 +1,30 @@
+package initializers
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+
+var DB *gorm.DB 
+
+func ConnectDB() () {
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	
+
+	var err error
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPassword, dbName)
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Error connecting to database")
+	}
+}
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func init() {
 	initializers.LoadEnvVariables()
+	initializers.ConnectDB()
 }
 
 func main() {
